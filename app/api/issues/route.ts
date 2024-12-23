3a67a256f2e8561ad423abba2e6bc136abdb7a88
David Ortet <davidortetsabater@gmail.com>
2024-12-23 11:16:01 +0100

    Handling Errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const issueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1).max(255),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required").max(255),
 });
 
 export async function POST(request: NextRequest) {
