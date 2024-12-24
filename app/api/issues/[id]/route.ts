061e148749f2f49849b1f9c36764f912d6c3fedc
David Ortet <davidortetsabater@gmail.com>
2024-12-24 10:49:06 +0100

    Building an API for Deleting Issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,3 +30,21 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: parseInt(id) },
+  });
+
+  return NextResponse.json({});
+}
