ab6f7e867e9194737fbd963f7be2c457299ba9a8
David Ortet <davidortetsabater@gmail.com>
2024-12-24 08:13:55 +0100

    Building a Styled Link Component

---
+++ app/components/Link.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link as RadixLink } from "@radix-ui/themes";
+import NextLink from "next/link";
+
+interface Props {
+  href: string;
+  children: string;
+}
+
+const Link = ({ href, children }: Props) => {
+  return (
+    <NextLink href={href} passHref legacyBehavior>
+      <RadixLink>{children}</RadixLink>
+    </NextLink>
+  );
+};
+
+export default Link;
