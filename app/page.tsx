dac1b9a152d74b27cf05baf9f637068f64b95435
David Ortet <davidortetsabater@gmail.com>
2024-12-27 13:30:15 +0100

    Implementing Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={1} />;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) {
+  const { page } = await searchParams;
+  return (
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(page)} />
+  );
 }
