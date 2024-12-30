d7a22419d4a2da1e4b8c615df66bce20d763c793
David Ortet <davidortetsabater@gmail.com>
2024-12-30 08:58:12 +0100

    Vuilding the IssueSummary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
-  return <LatestIssues />;
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
