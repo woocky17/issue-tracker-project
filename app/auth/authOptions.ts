b20a465d1d183c798d56e2da3bfc334722fdb677
David Ortet <davidortetsabater@gmail.com>
2024-12-31 08:39:59 +0100

    Fixing Google GignIn

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,25 +1,45 @@
-import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/prisma/client";
 import { NextAuthOptions } from "next-auth";
-import { CustomPrismaAdapter } from "./CustomPrismaAdapter";
 
-const authOptions: NextAuthOptions = {
-  adapter: CustomPrismaAdapter(prisma),
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
-  session: { strategy: "jwt" },
-};
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      try {
+        // Verificar si el usuario existe
+        const existingUser = await prisma.user.findUnique({
+          where: { email: user.email! },
+        });
 
-export default authOptions;
+        // Si el usuario no existe, lo creamos
+        if (!existingUser) {
+          await prisma.user.create({
+            data: {
+              name: user.name!,
+              email: user.email!,
+              image: user.image!,
+              createdAt: new Date(), // Asignamos la fecha de creación manualmente
+              updatedAt: new Date(), // Asignamos la fecha de actualización manualmente
+            },
+          });
+        }
+
+        return true; // Permite el inicio de sesión
+      } catch (error) {
+        console.error("Error en signIn callback:", error);
+        return false; // Bloquea el inicio de sesión en caso de error
+      }
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
